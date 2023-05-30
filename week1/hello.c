ce00e7c887e4f245a496b42742e0628503b8d8d6
Ruochun <y.ruochun@gmail.com>
2023-05-30 15:57:01 +0800

    Added user input option to print specific names Used cs50 header files for get_string function

--- week1/hello.c
+++ week1/hello.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
+
 int main(void) 
 {
-    printf ("hello, world \n");
+    string answer = get_string("What is your name?");
+    printf ("hello, %s\n", answer);
 }
