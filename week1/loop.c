848d1480ca1960c35041aa2bf79b7a99b81ac7d4
Nakachu <y.ruochun@gmail.com>
2023-06-02 22:16:04 +0800

    created loop.c

---
+++ week1/loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    //use while loop
+    int i = 0;
+    while (i < 3)
+    {
+        printf("喵呜\n");
+        i++;
+    }
+    
+    //use for loop
+    for (int k = 0; k < 3; k++)
+    {
+        printf("鲨呜\n");
+    }
+}
+
