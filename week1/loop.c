560d524bfa9302bea32337f9ae05bc5b5a1cc6bc
Nakachu <y.ruochun@gmail.com>
2023-06-02 22:21:52 +0800

    add user prompt with get_int for the number of loops

--- week1/loop.c
+++ week1/loop.c
@@ -3,16 +3,18 @@
 
 int main(void)
 {
+    //prompt user to enter the number
+    int i = get_int("喵呜多少次？");
+
     //use while loop
-    int i = 0;
-    while (i < 3)
+    while (i > 0)
     {
         printf("喵呜\n");
-        i++;
+        i--;
     }
-    
+
     //use for loop
-    for (int k = 0; k < 3; k++)
+    for (int k = get_int("鲨呜多少次？"); k > 0; k--)
     {
         printf("鲨呜\n");
     }
