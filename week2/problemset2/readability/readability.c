b197148328ae7fd9d56ce18a51c77b7f2cbbc733
Nakachu <y.ruochun@gmail.com>
2023-06-18 17:41:51 +0800

    completed count words function

--- week2/problemset2/readability/readability.c
+++ week2/problemset2/readability/readability.c
@@ -5,27 +5,29 @@
 
 string input(void);
 int count_letters(string);
-
+int count_words(string);
 
 int main(void) 
 {
     string text = input();
 
     int letters = count_letters(text);
-    printf ("letters: %d\n", letters);
+    printf("letters: %d\n", letters);
+
+    int words = count_words(text);
+    printf("words: %d\n", words);
 }
 
 //TO DO: prompt user imput
 
 string input(void)
 {
-    string input; int l;
-    l = strlen(input);
+    string input;
     do 
     {
-        input = get_string ("Text:");
+        input = get_string("Text:");
     }
-    while (l < 1);
+    while (strlen(input) < 1);
     return input;
 }
 
@@ -46,9 +48,37 @@ int count_letters(string text)
             }
     }
 
+
     return letters;
 }
 
+// TO DO: Count the number of words
+int count_words(string text)
+{
+    int space = 0; 
+    int sum = 0;
+    for (int i = 0, l = strlen(text); i < l; i++)
+    {
+        if ((text[i] == ' ') && (text[i - 1] != ' '))   //delete condition && (text[i + 1] != ' ') 
+        {
+            space = 1;
+        }
+        else                                            //I'm not sure about how to inlude cases such as spaces in the beginining or and as well as "..."
+        {
+            space = 0;
+        }
+
+        sum = sum + space;
+    }
+
+    int words = sum + 1;
+
+    return words;
+
+}
+
 //TO DO: calculate grade level using the Coleman-Liau intex: index = 0.0588 * L - 0.296 * S - 15.8
+
+
 
 // TO DO: Print grade level.
